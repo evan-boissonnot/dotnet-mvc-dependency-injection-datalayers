e8f213bbc70d0e81e047de6641592bf6160d369d
evan-boissonnot <commercial@boissonnot.Fr>
2019-02-22 14:46:24 +0100

    adding datalayers project

---
+++ LesDataLayers/Dal1.cs
@@ -0,0 +1,17 @@
+﻿using LesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesDataLayers
+{
+    public class Dal1 : IDal
+    {
+        public string SayAlelouia()
+        {
+            return "Not, I'm devil";
+        }
+    }
+}
---
+++ LesDataLayers/Dal2.cs
@@ -0,0 +1,17 @@
+﻿using LesInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LesDataLayers
+{
+    public class Dal2 : IDal
+    {
+        public string SayAlelouia()
+        {
+            return "Alelouia alelouia tralalalla";
+        }
+    }
+}
